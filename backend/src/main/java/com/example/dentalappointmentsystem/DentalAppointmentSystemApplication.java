3efa0e3abc26cd5d7507481a13a26aac8bc3267d
Bartek <bartekk.jonca@gmail.com>
2023-03-03 11:30:47 +0100

    Basic project structure and spring boot setup

---
+++ backend/src/main/java/com/example/dentalappointmentsystem/DentalAppointmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.dentalappointmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalAppointmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DentalAppointmentSystemApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/dentalappointmentsystem/DentalAppointmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dentalappointmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalAppointmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
