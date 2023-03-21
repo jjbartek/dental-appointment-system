eeca590375bb5d8f215f425da21fe151a4082461
Bartek <bartekk.jonca@gmail.com>
2023-03-21 21:12:18 +0100

    Add missing validation

--- backend/src/main/java/com/das/payloads/JwtAuthRequest.java
+++ backend/src/main/java/com/das/payloads/JwtAuthRequest.java
@@ -1,5 +1,7 @@
 package com.das.payloads;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +12,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class JwtAuthRequest {
-
+    @NotBlank(message = "Email cannot be empty")
+    @NotNull(message = "Email cannot be null")
     private String email;
+    @NotBlank(message = "Password cannot be empty")
+    @NotNull(message = "Password cannot be null")
     private String password;
 }
--- backend/src/main/java/com/das/payloads/RegisterRequest.java
+++ backend/src/main/java/com/das/payloads/RegisterRequest.java
@@ -1,5 +1,7 @@
 package com.das.payloads;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +12,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
+    @NotBlank(message = "Username cannot be empty")
+    @NotNull(message = "Username cannot be null")
     private String username;
+    @NotBlank(message = "Email cannot be empty")
+    @NotNull(message = "Email cannot be null")
     private String email;
+    @NotBlank(message = "Password cannot be empty")
+    @NotNull(message = "Password cannot be null")
     private String password;
 }
